47bd0d0b423c2e44cad9ccea7b45d235a70fbd74
dnovencido <dnovencido@gmail.com>
2022-11-05 21:34:30 +0800

    Revised home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
     def destroy
         session[:user_id] = nil
         flash[:notice] = "Logged out"
-        redirect_to root_path, status: :see_other
+        redirect_to _path, status: :see_other
     end
 end
